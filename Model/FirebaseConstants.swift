c3cdc3ab196bd52a9a7b1fcb705b24c7e97dd29e
Shaq-Mg <shaqmcg0@gmail.com>
2024-04-28 23:43:23 +0100

    FirebaseConstants added

---
+++ Model/FirebaseConstants.swift
@@ -0,0 +1,19 @@
+//
+//  FirebaseConstants.swift
+//  Peppy
+//
+//  Created by Shaquille McGregor on 28/04/2024.
+//
+
+import Foundation
+
+struct FirebaseConstants {
+    static let uid = "uid"
+    static let email = "email"
+    static let username = "username"
+    static let photoImageURL = "photoImageURL"
+    static let fromId = "fromId"
+    static let toId = "toId"
+    static let text = "text"
+    static let timestamp = "timestamp"
+}
