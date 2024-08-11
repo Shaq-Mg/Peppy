0022f9956baff7073d33dc70cf8820db45d70150
Shaq-Mg <shaqmcg0@gmail.com>
2024-08-11 19:58:25 +0100

    Initial Commit

---
+++ Peppy/PeppyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeppyApp.swift
+//  Peppy
+//
+//  Created by Shaquille McGregor on 11/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct PeppyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
