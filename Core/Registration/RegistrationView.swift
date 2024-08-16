5f34d5b2c3d30171d5bbaad46f64095fd8b5e40f
Shaq-Mg <shaqmcg0@gmail.com>
2024-08-16 17:04:41 +0100

    RegistrationView added

--- Core/Registration/RegistrationView.swift
+++ Core/Registration/RegistrationView.swift
@@ -9,8 +9,29 @@ import SwiftUI
 
 struct RegistrationView: View {
     @EnvironmentObject var viewModel: AuthenticationViewModel
+    @Environment(\.dismiss) var dismiss
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ZStack {
+                Color.accentColor.ignoresSafeArea()
+                VStack(spacing: 20) {
+                    VStack(spacing: 10) {
+                        InputView(text: $viewModel.name, title: "Name", placeholder: "Name", action: { viewModel.name = "" })
+                        
+                        InputView(text: $viewModel.email, title: "Email", placeholder: "User@hotmail.com", action: { viewModel.email = "" })
+                        
+                        InputView(text: $viewModel.password, title: "Password", placeholder: "password", action: { viewModel.password = "" }, isSecureField: true)
+                        
+                        InputView(text: $viewModel.confirmPassword, title: "Confirm Password", placeholder: "Confirm password", action: { viewModel.confirmPassword = "" }, isSecureField: true)
+                        
+                        buttonSection
+                            .padding(.top)
+                    }
+                }
+                .padding(.horizontal)
+                .onAppear { viewModel.clearLoginInformation() }
+            }
+        }
     }
 }
 
@@ -19,6 +40,39 @@ struct RegistrationView_Previews: PreviewProvider {
         NavigationStack {
             RegistrationView()
                 .environmentObject(AuthenticationViewModel())
+        }
+    }
+}
+
+extension RegistrationView {
+    private var buttonSection: some View {
+        VStack(spacing: 16) {
+            RegistrationButton(title: "Create account") {
+                Task {
+                    do {
+                        if formIsValid {
+                            try await viewModel.signUp(withEmail: viewModel.email, name: viewModel.name, password: viewModel.password, confirmPassword: viewModel.confirmPassword)
+                            
+                            viewModel.loginStatusMessage = "Successfully created user: \(viewModel.userSession?.uid)"
+                        }
+                    } catch {
+                        print("Failed to login user \(error)")
+                        viewModel.loginStatusMessage = "Failed to login user: \(error)"
+                    }
+                }
+            }
+            .disabled(!formIsValid)
+            
+            Button("Already have a account? Login") {
+                dismiss()
+            }
+            .font(.callout)
+            .foregroundStyle(.black)
+            
+            Text(viewModel.loginStatusMessage)
+                .padding(!viewModel.loginStatusMessage.isEmpty ? 8 : 0)
+                .background(RoundedRectangle(cornerRadius: 5).foregroundStyle(.red))
+                .shadow(radius: 2)
         }
     }
 }
