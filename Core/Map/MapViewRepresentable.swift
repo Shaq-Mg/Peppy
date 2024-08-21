454f94db0c18c69cbcbeac46d61e4c93868e6e09
Shaq-Mg <shaqmcg0@gmail.com>
2024-08-21 11:17:37 +0100

    Getting location coordinates

--- Core/Map/MapViewRepresentable.swift
+++ Core/Map/MapViewRepresentable.swift
@@ -23,7 +23,9 @@ struct MapViewRepresentable: UIViewRepresentable {
     }
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
-        
+        if let coordinate = locationViewModel.selectedLocationCoordinate {
+            context.coordinator.addAndSelectAnnotation(withCoordinate: coordinate)
+        }
     }
     
     func makeCoordinator() -> MapCoordinator {
@@ -34,17 +36,52 @@ struct MapViewRepresentable: UIViewRepresentable {
 extension MapViewRepresentable {
     
     class MapCoordinator: NSObject, MKMapViewDelegate {
+        
+        // MARK: - Properties
         let parent: MapViewRepresentable
         
+        // MARK: - Lifecycle
         init(parent: MapViewRepresentable) {
             self.parent = parent
             super.init()
         }
         
+        // MARK: - MKMapViewDelegate
         func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
             let region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: userLocation.coordinate.latitude, longitude: userLocation.coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05))
             
             parent.mapView.setRegion(region, animated: true)
+        }
+        
+        // MARK: - Helpers
+        func addAndSelectAnnotation(withCoordinate coordinate: CLLocationCoordinate2D) {
+            parent.mapView.removeAnnotations(parent.mapView.annotations)
+            
+            let anno = MKPointAnnotation()
+            anno.coordinate = coordinate
+            self.parent.mapView.addAnnotation(anno)
+            self.parent.mapView.selectAnnotation(anno, animated: true)
+            
+            parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
+        }
+        
+        func getDestinationRoute(from userLocation: CLLocationCoordinate2D, to destination: CLLocationCoordinate2D, completion: @escaping (MKRoute) -> ()) {
+            let userPlacemark = MKPlacemark(coordinate: userLocation)
+            let destPlacemark = MKPlacemark(coordinate: destination)
+            let request = MKDirections.Request()
+            request.source = MKMapItem(placemark: userPlacemark)
+            request.destination = MKMapItem(placemark: destPlacemark)
+            let directions = MKDirections(request: request)
+            
+            directions.calculate { response, error in
+                if let error = error {
+                    print("DEBUG: Failed to get directions with error \(error.localizedDescription)")
+                    return
+                }
+                
+                guard let route = response?.routes.first else { return }
+                completion(route)
+            }
         }
     }
 }
--- Core/Map/ViewModel/LocationSearchViewModel.swift
+++ Core/Map/ViewModel/LocationSearchViewModel.swift
@@ -11,7 +11,7 @@ import MapKit
 class LocationSearchViewModel: NSObject, ObservableObject {
     // MARK: = Properties
     @Published var results = [MKLocalSearchCompletion]()
-    @Published var selectedLocation: String?
+    @Published var selectedLocationCoordinate: CLLocationCoordinate2D?
     
     private let searchCompleter = MKLocalSearchCompleter()
     var queryFragment: String = "" {
@@ -21,6 +21,7 @@ class LocationSearchViewModel: NSObject, ObservableObject {
     }
     
     // MARK: - Lifecycle
+    
     override init() {
         super.init()
         searchCompleter.delegate = self
@@ -28,10 +29,31 @@ class LocationSearchViewModel: NSObject, ObservableObject {
     }
     
     // MARK: - Helpers
-    func selectedLocation(_ location: String) {
-        self.selectedLocation = location
+    
+    func selectedLocation(_ localSearch: MKLocalSearchCompletion) {
+        locationSearch(forLocalSearchCompletion: localSearch) { response, error in
+            if let error = error {
+                print("DEBUG: Location search failed with error \(error.localizedDescription)")
+                return
+            }
+            guard let item = response?.mapItems.first else { return }
+            let coordinate = item.placemark.coordinate
+            self.selectedLocationCoordinate = coordinate
+            print("DEBUG: Location coordinates \(coordinate)")
+        }
+    }
+    
+    func locationSearch(forLocalSearchCompletion localSearch: MKLocalSearchCompletion,
+                         completion: @escaping MKLocalSearch.CompletionHandler) {
+        let searchRequest = MKLocalSearch.Request()
+        searchRequest.naturalLanguageQuery = localSearch.title.appending(localSearch.subtitle)
+        let search = MKLocalSearch(request: searchRequest)
+        
+        search.start(completionHandler: completion)
     }
 }
+
+// MARK: - MKLocalSearchCompleterDelegate
 
 extension LocationSearchViewModel: MKLocalSearchCompleterDelegate {
     func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
