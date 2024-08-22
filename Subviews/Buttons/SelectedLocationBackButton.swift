8db1257e1c1b0df0f118152b843c0a6df2397a2a
Shaq-Mg <shaqmcg0@gmail.com>
2024-08-22 12:00:59 +0100

    SelectLocationBackButton feature added for locationSelectedState

---
+++ Subviews/Buttons/SelectedLocationBackButton.swift
@@ -0,0 +1,18 @@
+//
+//  SelectedLocationBackButton.swift
+//  Peppy
+//
+//  Created by Shaquille McGregor on 22/08/2024.
+//
+
+import SwiftUI
+
+struct SelectedLocationBackButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectedLocationBackButton()
+}
