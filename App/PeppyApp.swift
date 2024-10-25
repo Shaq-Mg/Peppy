671e6f1d67430e5d11736ab3f8a72e44c4abe389
Shaq-Mg <shaqmcg0@gmail.com>
2024-10-25 17:57:32 +0100

    SETUP: Configured firebase

---
+++ App/PeppyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeppyApp.swift
+//  Peppy
+//
+//  Created by Shaquille McGregor on 25/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct PeppyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
