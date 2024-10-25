e469b95722168e68235c4cf5065ef5c36c03a22a
Shaq-Mg <shaqmcg0@gmail.com>
2024-10-25 17:26:31 +0100

    Initial Commit

---
+++ PeppyTests/PeppyTests.swift
@@ -0,0 +1,17 @@
+//
+//  PeppyTests.swift
+//  PeppyTests
+//
+//  Created by Shaquille McGregor on 25/10/2024.
+//
+
+import Testing
+@testable import Peppy
+
+struct PeppyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
