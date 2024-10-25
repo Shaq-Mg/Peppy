29fb248c6131a96c4ddd30275be9a7e34f71cbbd
Shaq-Mg <shaqmcg0@gmail.com>
2024-10-25 21:56:05 +0100

    UPDATE: SettingsView UI added

---
+++ Subviews/Cells/SettingsCellView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsCellView.swift
+//  Peppy
+//
+//  Created by Shaquille McGregor on 25/10/2024.
+//
+
+import SwiftUI
+
+struct SettingsCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsCellView()
+}
