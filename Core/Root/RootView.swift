90aea1f5c61f7b0193e721d0f7e7ffb48447901c
Shaq-Mg <shaqmcg0@gmail.com>
2024-10-27 16:32:58 +0000

    Functions and publishers added to AuthViewModel & RootView created to update UI whether userSession publisher is == nil 🤙

---
+++ Core/Root/RootView.swift
@@ -0,0 +1,18 @@
+//
+//  RootView.swift
+//  Peppy
+//
+//  Created by Shaquille McGregor on 27/10/2024.
+//
+
+import SwiftUI
+
+struct RootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootView()
+}
